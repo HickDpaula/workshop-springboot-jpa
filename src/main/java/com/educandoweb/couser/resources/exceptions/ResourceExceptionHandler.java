0060ae4ba4eac5194110035b3fb53ce84d4b44b7
Henrique Santos <hickdpaula@gmail.com>
2023-12-14 15:44:59 -0300

    Update Exception handling - delete

--- src/main/java/com/educandoweb/couser/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/couser/resources/exceptions/ResourceExceptionHandler.java
@@ -14,16 +14,18 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError err = new StandardError(Instant.now(), status.value(),  error, e.getMessage(), request.getRequestURI());
-         return ResponseEntity.status(status).body(err);
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
     }
+
     @ExceptionHandler(DataBaseException.class)
-    public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request) {
         String error = "Database error";
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        StandardError err = new StandardError(Instant.now(), status.value(),  error, e.getMessage(), request.getRequestURI());
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
+    }
 }
